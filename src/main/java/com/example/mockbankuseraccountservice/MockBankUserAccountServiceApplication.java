689990159742419788a818886330d1c369307b82
druiz35 <escobarruizd@gmail.com>
2026-02-22 10:42:08 -0500

    Initial commit

---
+++ src/main/java/com/example/mockbankuseraccountservice/MockBankUserAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockbankuseraccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockBankUserAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockBankUserAccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mockbankuseraccountservice/MockBankUserAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockbankuseraccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockBankUserAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
