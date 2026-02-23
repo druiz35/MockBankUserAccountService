caf693b1f8d9c99e7762539859e293ca136b7549
druiz35 <escobarruizd@gmail.com>
2026-02-22 23:32:47 -0500

    feat: Added config for DTO to Entity transformations

--- src/main/java/com/druiz/mockbankuseraccountservice/config/UserAccountModelConfig.java
+++ src/main/java/com/druiz/mockbankuseraccountservice/config/UserAccountModelConfig.java
@@ -1,4 +1,13 @@
 package com.druiz.mockbankuseraccountservice.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserAccountModelConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
